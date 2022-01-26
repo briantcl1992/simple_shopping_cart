e386cfe64b8a55da856cf986d6bbda9857560685
clbt2 <tongchinglim@gmail.com>
2022-01-26 15:11:56 +0800

    added add-item and delete-item functions

--- js/application.js
+++ js/application.js
@@ -11,11 +11,13 @@ var updateItemValue = function(ele) {
     }
     return itemValue;
 };
+
 var sum = function (acc, x) { return acc + x;};
+
 var updateTotalPrice = function() {
     var itemValues = [];
     $('.item').each(function(i,ele){
-        console.log(ele);
+        //console.log(ele);
         var itemValue = updateItemValue(ele);
         itemValues.push(itemValue);
     })
@@ -25,10 +27,49 @@ var updateTotalPrice = function() {
     });
     var totalPrice = itemValuesClean.reduce(sum);
     console.log(totalPrice);
-    $('#total-price').html(totalPrice);
+    if (totalPrice !=0){
+        $('#total-price').html("$ " + totalPrice);
+    } else{
+        $('#total-price').html("$--.--");
+    }
 }
 
 $( document ).ready(function() {
     console.log("ready")
     updateTotalPrice();
+    
+    $(document).on('keyup', '.quantity', function(){
+        updateTotalPrice();
+    });
+
+    $(document).on('click','#fork',function(){
+        var name = $('#name').val();
+        var cost = $('#cost').val();
+        $('#item-list').prepend('<div class="row item">\
+        <div class="item-name col-xs-3">\
+          '+ name +'\
+        </div>\
+        <div class="item-price col-xs-3">\
+          $' + cost + '.00 \
+        </div> \
+        <div class="item-qty col-xs-3"> \
+          <label>QTY</label> \
+          <input class="quantity" type="number"> \
+        </div> \
+        <div class="col-xs-1"> \
+          <button class="remove"> \
+            Remove \
+          </button> \
+        </div> \
+        <div class="item-subtotal col-xs-2"> \
+        $--.-- \
+        </div> \
+      </div>');
+    });
+
+    $(document).on('click','.remove',function(){
+        $(this).parents('.row').remove();
+        updateTotalPrice();
+    })
+
 })
